7228aeeae2ef405f4be10ce9793d9c8d344ad833
guzamak <120812215+guzamak@users.noreply.github.com>
2025-04-22 22:18:55 +0700

    Update timer.jsx

--- components/timer.jsx
+++ components/timer.jsx
@@ -69,6 +69,9 @@ export default function Timer() {
 
   useEffect(()=>{
     setClockDeg(0)
+    setHour(hourInput)
+    setMinute(minuteInput)
+    setSecond(secondInput)
   },[hourInput,minuteInput,secondInput])
 
   useEffect(() => {
